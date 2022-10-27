765aa7bb6a6eb6d94e02d0a1f8196e55cd92f2ce
Lasagnanator <d96a07@gmail.com>
2022-10-27 15:56:28 +0200

    Added sorting function to index

--- index.py
+++ index.py
@@ -11,5 +11,32 @@ db = firestore.client()
 users_ref = db.collection(u'users')
 docs = users_ref.stream()
 
-for doc in docs:
-    print(f'{doc.id} => {doc.to_dict()}')
+
+def get_names():
+    for doc in docs:
+        print(f'{doc.id} => {doc.to_dict()[("name")]}')
+    #    print(f'{doc.id} => {doc.to_dict()}')
+
+
+def add_lovelace():
+    doc_ref = db.collection(u'users').document(u'alovelace')
+    doc_ref.set({
+        u'name': u'Ada',
+        u'surname': u'Lovelace',
+        u'date': 1815
+    })
+
+
+def first_two():
+    users = db.collection('users')
+    query = users.order_by('name').limit_to_last(2)
+    results = query.get()
+    for person in results:
+        print(person.to_dict()[('name')])
+
+
+get_names()
+print("\n~~~~~~~~~~\n")
+# add_lovelace()
+# print("\n~~~~~~~~~~\n")
+first_two()
