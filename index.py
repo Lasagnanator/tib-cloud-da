ce7ccc5f4430b9359b8421f73422311b65ec6c6f
Lasagnanator <d96a07@gmail.com>
2022-10-27 15:08:41 +0200

    Index now retirieves all records

--- index.py
+++ index.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env python
 
-import firebase
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+cred = credentials.Certificate("./firestore-credentials.json")
+app = firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+users_ref = db.collection(u'users')
+docs = users_ref.stream()
+
+for doc in docs:
+    print(f'{doc.id} => {doc.to_dict()}')
